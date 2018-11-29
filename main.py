1a1611f25f6713c129facba8e9b30a6f493f5938
Jean Bilheux <bilheuxjm@ornl.gov>
2018-11-29 16:59:10 -0500

    up to date with version on windows

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fbs_runtime.application_context import ApplicationContext, cached_property
 from subprocess import check_output
 import subprocess
 import glob
@@ -17,15 +18,29 @@ except ImportError:
     from PyQt5 import QtCore, QtGui
     from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QTableWidgetItem, QDialog, QProgressBar
 
-from ui.main_window import Ui_MainWindow as UiMainWindow
-from ui.configuration import Ui_Dialog as UiDialog
+from main_window import Ui_MainWindow as UiMainWindow
+from configuration import Ui_Dialog as UiDialog
 
 user_home = expanduser("~")
 CONFIG_FILE = os.path.join(user_home, '.mcp_config')
-EXE_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'code', 'TPX_CubeRead.exe')
+#EXE_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'code', 'TPX_CubeRead.exe')
 
 LIST_PREFIX_TO_COPY = ['ShutterCount.txt', 'ShutterTimes.txt', 'Spectra.txt', 'SummedImg.fits']
 DEBUG = False
+NBR_TRY_RUNNING_CORRECTION = 3
+
+class AppContext(ApplicationContext):
+
+    exe_file = ''
+
+    def run(self):
+        self.exe_file = self.get_resource('TPX_CubeRead.exe')
+        self.window.show()
+        return self.app.exec_()
+
+    @cached_property
+    def window(self):
+        return Interface(self.exe_file)
 
 
 class Interface(QMainWindow):
@@ -34,11 +49,17 @@ class Interface(QMainWindow):
     config_output_folder = ''
     config_time_out = 35
 
+    min_time_out = 5
+    max_time_out = 120
+
     list_folders = []
 
     tutorial_web_link = "https://neutronimaging.pages.ornl.gov/"
+    exe_file = ''
 
-    def __init__(self, parent=None):
+    def __init__(self, exe_file, parent=None):
+
+        self.exe_file = exe_file
 
         QMainWindow.__init__(self, parent=parent)
         self.ui = UiMainWindow()
@@ -47,7 +68,13 @@ class Interface(QMainWindow):
         self.statusbar()
 
         self.load_config_file()
+        self.populate_list_of_folders_widgets()
         self.check_run_correction_button_status()
+
+
+    def populate_list_of_folders_widgets(self):
+        config_working_folder = self.config_working_folder
+        self.populate_list_of_working_folders(parent_folder=config_working_folder)
 
     # Event handlers
 
@@ -86,8 +113,32 @@ class Interface(QMainWindow):
 
     def add_logbook(self, text):
         self.ui.logbook.append(text)
+        QApplication.processEvents()
+
+    def clear_logbook(self):
+        self.ui.logbook.clear()
+
+    @staticmethod
+    def get_number_of_files_in_folder(folder):
+        list_files = glob.glob(os.path.join(folder, '*'))
+        return len(list_files)
+
+    @staticmethod
+    def get_number_of_files_expected(first_file_name):
+        dirname = os.path.dirname(first_file_name)
+        basename = os.path.basename(first_file_name)
+        basename_split = basename.split('_')
+        basename_prefix = "_".join(basename_split[:-1])
+        list_files = glob.glob(os.path.join(dirname, (basename_prefix + '*.fits')))
+        return len(list_files) - 1  # -1 because the SummedImg is not corrected by Anton's code
 
     def run_correction_clicked(self):
+
+        self.clear_logbook()
+
+        # switch directly to logbook tab
+        self.ui.tabWidget.setCurrentIndex(1)
+
         list_folders_selected = self.get_list_folders_selected()
         output_folder = self.config_output_folder
         self.add_logbook("output folder selected is {}".format(output_folder))
@@ -107,6 +158,8 @@ class Interface(QMainWindow):
 
         # built command lines
         counter = 1
+        list_of_errors = []
+
         for _folder in list_folders_selected:
             self.add_logbook("> Working with folder: {}".format(_folder))
             _first_file_full_name = self.get_first_file_full_name(folder=_folder)
@@ -115,33 +168,77 @@ class Interface(QMainWindow):
 
             # create folder if not there yet
             if not os.path.exists(local_output_folder):
-                if not DEBUG:
-                    os.mkdir(local_output_folder)
+                self.add_logbook(" output folder does not exist yet!")
+                os.mkdir(local_output_folder)
+                self.add_logbook(" output folder has been created!")
 
             for _first_file in _first_file_full_name:
 
-                self.add_logbook("-> Working with first file: {}".format(_first_file))
-                cmd = EXE_FILE + ' "{}"'.format(_first_file)
+                time_out = np.float(self.config_time_out)
+                nbr_files_expected = Interface.get_number_of_files_expected(_first_file)
+
+                self.add_logbook("-> Working with file: {}".format(_first_file))
                 self.add_logbook("  Running command line!")
+                cmd = self.exe_file + ' "{}" > nul'.format(_first_file)
                 self.add_logbook("  cmd: {}".format(cmd))
 
-                try:
-                    if not DEBUG:
-                        subprocess.run(cmd, shell=True, check=True, timeout=self.config_time_out)
-                except:
-                    pass
+                correction_folder_full_name = os.path.join(os.path.dirname(_first_file), 'Corrected')
+                process_failed = True
+                index_try = 0
+                while process_failed:
+                    try:
+                        self.add_logbook(" Time_out: {}s".format(time_out))
+                        subprocess.run(cmd, shell=False, check=True, timeout=time_out)
+                    except:
+                        pass
+
+                    # checking that output file is there
+                    self.add_logbook(" Checking that all conditions are met to keep going:")
+                    if not os.path.exists(correction_folder_full_name):
+                        # correction folder does not exist !
+                        self.add_logbook(" - correction folder exists? NO!  :(!")
+                    else:
+                        self.add_logbook(" - correction folder exists? YES! :)!")
+                        # correction folder exists
+                        # let's check if the number of files in it is correct
+                        nbr_files_in_folder = Interface.get_number_of_files_in_folder(correction_folder_full_name)
+                        self.add_logbook(" - number of files created / expected = {}/{}".format(nbr_files_in_folder,
+                                                                                                nbr_files_expected))
+                        if not (nbr_files_in_folder == nbr_files_expected):
+                            time_out *= 2
+                            index_try += 1
+                        else:
+                            process_failed = False
+
+                    if index_try == NBR_TRY_RUNNING_CORRECTION:
+                        _error = {'folder': _folder,
+                                  'first_file_name': _first_file,
+                                  'time_out': time_out}
+                        list_of_errors.append(_error)
+                        counter += 3
+                        self.add_logbook(" No more try! Command failed. Let's move on to the next images or folder!")
+                        break
+
+                # still failing, we can jump to the next set of images to correct
+                if process_failed:
+
+                    # removing file created by Anton's code
+                    self.add_logbook("   Remove input corrected folder {}".format(correction_folder_full_name))
+                    shutil.rmtree(correction_folder_full_name)
+                    continue
 
                 counter += 1
                 self.ui.eventProgress.setValue(counter)
                 QApplication.processEvents()
 
                 # full name of folder created by code
-                correction_folder_full_name = os.path.join(os.path.dirname(_first_file), 'Corrected')
-                self.add_logbook("  Full name of folder created by Anton's code: {}".format(correction_folder_full_name))
+                self.add_logbook("  Full name of folder created by Anton's " +
+                                 "code: {}".format(correction_folder_full_name))
 
                 # get file base name
                 _base_file_name_for_folder_creation = self.get_base_file_name(_first_file)
-                self.add_logbook(" _ base_file_name_for_folder_creation is: {}".format(_base_file_name_for_folder_creation))
+                self.add_logbook(" - base_file_name_for_folder_creation is: " +
+                                 "{}".format(_base_file_name_for_folder_creation))
 
                 # rename Corrected folder into final name
                 # if folder already there, remove it first
@@ -150,8 +247,10 @@ class Interface(QMainWindow):
 
                 # make sure the output folder does not exist yet
                 if os.path.exists(data_set_folder):
+                    self.add_logbook(" Output folder does already exist!")
                     if not DEBUG:
                         shutil.rmtree(data_set_folder)
+                        self.add_logbook(" Removed output folder!")
 #                correction_folder_new_location_full_name = os.path.join(local_output_folder, 'Corrected')
 
                 counter += 1
@@ -166,8 +265,11 @@ class Interface(QMainWindow):
                 self.add_logbook("  Moving Corrected folder to final location")
                 self.add_logbook("   - full input folder name: {}".format(correction_folder_full_name))
                 self.add_logbook("   - full output folder name: {}".format(data_set_folder))
-                if not DEBUG:
+                if os.path.exists(correction_folder_full_name):
+                    self.add_logbook(" correction folder exists!")
                     shutil.copytree(correction_folder_full_name, data_set_folder)
+                    self.add_logbook(" correction folder has been copied to output folder")
+
                 ##shutil.move(correction_folder_full_name, local_output_folder)
 
                 counter += 1
@@ -192,8 +294,7 @@ class Interface(QMainWindow):
 
                 # remove Corrected folder created by Anton's code
                 self.add_logbook("   Remove input corrected folder {}".format(correction_folder_full_name))
-                if not DEBUG:
-                    shutil.rmtree(correction_folder_full_name)
+                shutil.rmtree(correction_folder_full_name)
 
                 counter += 1
                 self.ui.eventProgress.setValue(counter)
@@ -201,10 +302,22 @@ class Interface(QMainWindow):
 
                 QApplication.processEvents()
 
+                self.add_logbook("")
+
         self.ui.eventProgress.setVisible(False)
         QApplication.processEvents()
 
+        self.add_logbook("DONE!")
         QApplication.restoreOverrideCursor()
+
+        if not list_of_errors == []:
+            self.add_logbook("")
+            self.add_logbook("List of folders/images for which the correction did not work!")
+            for _entry in list_of_errors:
+                self.add_logbook(" - Folder: {}".format(_entry['folder']))
+                self.add_logbook(" - First File: {}".format(_entry['first_file_name']))
+                self.add_logbook(" - Time Out: {} s".format(_entry['time_out']))
+                self.add_logbook(" ----------------------------------------------------")
 
     def help_clicked(self):
         import webbrowser
@@ -244,7 +357,7 @@ class Interface(QMainWindow):
             with open(CONFIG_FILE, 'rb') as handle:
                 _cfg = pickle.load(handle)
 
-#                print(_cfg['working_folder'])
+                #print(_cfg['working_folder'])
                 try:
                     self.config_working_folder = _cfg['working_dir']
                     self.config_output_folder = _cfg['output_dir']
@@ -442,12 +555,17 @@ class Configuration(QDialog):
         """retrieve the hidden config ascii file (if there) and update the ui"""
         self.ui.input_folder_label.setText(self.working_folder)
         self.ui.output_folder_label.setText(self.output_folder)
+        self.ui.time_out_slider.setMinimum(self.parent.min_time_out)
+        self.ui.time_out_slider.setMaximum(self.parent.max_time_out)
         self.ui.time_out_slider.setValue(np.int(self.parent.config_time_out))
         self.ui.time_out_label.setText(str(self.parent.config_time_out))
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    o_interface = Interface()
-    o_interface.show()
-    sys.exit(app.exec_())
+    app = AppContext()
+    exit_code = app.run()
+    sys.exit(exit_code)
+    #app = QApplication(sys.argv)
+    #o_interface = Interface()
+    #o_interface.show()
+    #sys.exit(app.exec_())
